1a03c07959656f7f0054866999f0c336f8ffaca9
Thiago Mazzi <thiagodev728@gmail.com>
2025-02-04 15:27:01 -0300

    Instalando os pacotes do NuGet

---
+++ ListaTarefasAPI/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace ListaTarefasAPI.Models
+{
+    public class Tarefa
+    {
+        public int tarefaId { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluída { get; set; }
+        public int TipoTarefaId { get; set; }
+        public TipoTarefa? TipoTarefa { get; set; }
+    }
+}
